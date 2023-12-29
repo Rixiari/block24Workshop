e115d5110a4b9d38f93d096388c4db09472d504b
Rixiari <rixiari@LAPTOP-E4RKFESK>
2023-12-28 23:47:48 -0500

    Block 24 Workshop: Puppy Pals

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react';
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+const featuredPup = puppies.find((pup) =>pup.id ===featPupId)
+console.log(featuredPup);
+
   return (
     <>
-      <div>
-
+      <div className = "App">
+        {
+          puppies.map((puppy) =>{
+            return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+          })
+        }
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
